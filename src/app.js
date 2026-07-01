3b61f8680050ccd09d8e723b120865944a939225
bwhtsu <liliiyazh5@gmail.com>
2026-07-01 19:25:20 +0300

    home work

---
+++ src/app.js
@@ -0,0 +1,121 @@
+const URL = "https://pixabay.com/api/";
+const API_KEY = "56145635-1dfedf23379adae5fa585b845";
+
+let search = "";
+let limit = 12;
+let page = 1;
+let per_page = 20;
+
+const formEl = document.querySelector(".search-form");
+const galeryEl = document.querySelector(".gallery");
+const divEl = document.querySelector(".elements")
+
+
+// fetch(`${URL}?key=${API_KEY}&q=${search}&page=${page}&per_page=${per_page}`)
+// .then(res => res.json())
+// .then(res => console.log(res))
+
+async function getImages(search, page) {
+    const res = await fetch(`${URL}?key=${API_KEY}&q=${search}&page=${page}&per_page=${limit}`,);
+
+    const data = await res.json();
+    return data;
+}
+
+formEl.addEventListener("submit", async(e) => {
+
+    e.preventDefault();
+    search = e.currentTarget.elements.query.value;
+    
+    const res = await getImages(search, page);
+    // console.log(getImages(search, page));
+    
+    await renderImages(res.hits)
+
+
+})
+
+
+// Тобі цікаві такі властивості:
+
+// webformatURL - посилання на маленьке зображення для списку карток
+// largeImageURL - посилання на велике зображення (дивись пункт 'додатково')
+// likes - кількість лайків
+// views - кількість переглядів
+// comments - кількість коментарів
+// downloads - кількість завантажень
+
+
+function renderImages(array) {
+
+        const item = array.map(({webformatURL, largeImageURL, likes, views, comments, downloads, tags}) => {
+            return `    
+                <li class="photo-card">
+
+  <img src="${webformatURL}" alt="${tags}" />
+
+
+
+  <div class="stats">
+
+    <p class="stats-item">
+
+      <i class="material-icons">thumb_up</i>
+
+      ${likes}
+
+    </p>
+
+    <p class="stats-item">
+
+      <i class="material-icons">visibility</i>
+
+      ${views}
+
+    </p>
+
+    <p class="stats-item">
+
+      <i class="material-icons">comment</i>
+
+      ${comments}
+
+    </p>
+
+    <p class="stats-item">
+
+      <i class="material-icons">cloud_download</i>
+
+      ${downloads}
+
+    </p>
+
+  </div>
+
+</li>`
+    }).join("");
+
+    galeryEl.insertAdjacentHTML("beforeend", item)
+}
+
+
+const observer = new IntersectionObserver((entry) => {
+
+    console.log(entry);
+    
+    entry.forEach(async (e) => {
+        
+        if(e.isIntersecting && search !== ""){
+            page += 1;
+            const res = await getImages(search, page);
+    
+            await renderImages(res.hits)
+        }
+
+    })
+
+}, {
+    rootMargin: "200px"
+})
+
+observer.observe(divEl);
